7c033e7421556a9e092d585836ac85a91fdeda58
Freeman <trulyn300@gmail.com>
2025-11-23 13:02:01 -0300

    Atualizar o raw.js

--- api/raw.js
+++ api/raw.js
@@ -1,17 +1,16 @@
 import { kv } from "@vercel/kv";
 
 export default async function handler(req, res) {
+    const filename = req.url.replace("/api/raw/", "");
     try {
-        const filename = req.url.replace("/raw/", "");
         const file = await kv.get(`file_${filename}`);
-
         if (!file) return res.status(404).send("Arquivo não encontrado.");
 
+        // Incrementa execuções
         await kv.incr(`exec_${filename}`);
 
         res.setHeader("Content-Type", "text/plain");
         return res.send(file);
-
     } catch (e) {
         return res.status(500).send("Erro interno.");
     }
